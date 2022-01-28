a5f9f1f28ac5f693cb8b6ed6bbfd6c9a5e651a9a
fachrz <rfachru3@outlook.com>
2022-01-28 11:59:26 +0700

    ubah tipedata nim

--- src/Mahasiswa.java
+++ src/Mahasiswa.java
@@ -1,11 +1,11 @@
 import java.util.Date;
 
 public class Mahasiswa {
-    protected int nim;
+    protected String nim;
     protected String nama;
     protected Date tanggal;
 
-    public boolean setData(int nim, String nama){
+    public boolean setData(String nim, String nama){
         this.nim = nim;
         this.nama = nama;
         this.tanggal = new Date();
@@ -14,9 +14,7 @@ public class Mahasiswa {
     }
 
     public String[] showData(){
-        Integer nim = this.nim;
-        String[] array = {nim.toString(), this.nama, this.tanggal.toString()};
-
-        return array;
+        String[] data = {this.nim, this.nama, this.tanggal.toString()};
+        return data;
     }
 }
